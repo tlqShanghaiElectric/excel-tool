db94fd37a6b20c42aab936a1797bfeb2ef3e02ef
tlqShanghaiElectric <tanglq2@shanghai-electric.com>
2018-06-08 16:09:33 +0800

    change the file to function

--- getCPPStruct.py
+++ getCPPStruct.py
@@ -7,51 +7,53 @@ Created on Fri May 25 10:51:24 2018
 
 import openpyxl
 
-typeDict = {"bool":"BOOL", "double":"LREAL", "float": "REAL", \
-            "int":"DINT", "unsigned int":"DWORD"}
+def convertToVar():
 
-fileName = "g_pChannelData.xlsx"
-workbook = openpyxl.load_workbook(fileName)
-sheet1 = workbook["Sheet1"]
-# cell = sheet1.cell(row=3, column=4)
-# print(sheet1.max_row)
+    typeDict = {"bool":"BOOL", "double":"LREAL", "float": "REAL", \
+                "int":"DINT", "unsigned int":"DWORD"}
 
-structString = ""
-newStructTypeName = "g_ChannelDataFull"
-newStructName = "g_ChannelDataFullName"
-structBeginString = "TYPE" + "\n" + "\t" + newStructName \
-                    + " : STRUCT" + "\n"
-structMainString = ""
-for i in range(2, sheet1.max_row+1):
-    cellForLongName = sheet1.cell(row = i, column = 4)
-    longName = cellForLongName.value
-    cellForType = sheet1.cell(row = i, column = 2)
-    typeName = cellForType.value
-    cellForShortName = sheet1.cell(row = i, column = 5)
-    shortName = cellForShortName.value
-    name = longName if len(longName) < 32 else shortName
-    if "[" not in name:
-        structMainString += "\t" * 2 + name + " : " \
-                            + typeDict.get(typeName)  + ";\n"
-    else:
-        index1 = name.find("[")
-        index2 = name.find("]")
-        num = name[index1+1: index2]
-        structMainString += "\t" * 2 + name[:index1] + " : " \
-                            + "ARRAY[0.." + num + "] OF " \
-                            + typeDict.get(typeName) + ";\n"
+    fileName = "g_pChannelData.xlsx"
+    workbook = openpyxl.load_workbook(fileName)
+    sheet1 = workbook["Sheet1"]
+    # cell = sheet1.cell(row=3, column=4)
+    # print(sheet1.max_row)
 
-structEndString = "\t" + "END_STRUCT;\n" + "END_TYPE"
-structString = structBeginString + structMainString + structEndString
+    structString = ""
+    newStructTypeName = "g_ChannelDataFull"
+    newStructName = "g_ChannelDataFullName"
+    structBeginString = "TYPE" + "\n" + "\t" + newStructName \
+                        + " : STRUCT" + "\n"
+    structMainString = ""
+    for i in range(2, sheet1.max_row+1):
+        cellForLongName = sheet1.cell(row = i, column = 4)
+        longName = cellForLongName.value
+        cellForType = sheet1.cell(row = i, column = 2)
+        typeName = cellForType.value
+        cellForShortName = sheet1.cell(row = i, column = 5)
+        shortName = cellForShortName.value
+        name = longName if len(longName) < 32 else shortName
+        if "[" not in name:
+            structMainString += "\t" * 2 + name + " : " \
+                                + typeDict.get(typeName)  + ";\n"
+        else:
+            index1 = name.find("[")
+            index2 = name.find("]")
+            num = name[index1+1: index2]
+            structMainString += "\t" * 2 + name[:index1] + " : " \
+                                + "ARRAY[0.." + num + "] OF " \
+                                + typeDict.get(typeName) + ";\n"
+
+    structEndString = "\t" + "END_STRUCT;\n" + "END_TYPE"
+    structString = structBeginString + structMainString + structEndString
 
 
-with open("Types.typ", 'w') as f:
-    f.write(structString)
+    with open("Types.typ", 'w') as f:
+        f.write(structString)
 
-#connect struct name to struct type
-connectionLanguage = "VAR\n\t" + newStructTypeName + " : " \
-                    + newStructName + ";\n" + "END_VAR"
-with open("Variables.var", 'w') as f:
-    f.write(connectionLanguage)
+    #connect struct name to struct type
+    connectionLanguage = "VAR\n\t" + newStructTypeName + " : " \
+                        + newStructName + ";\n" + "END_VAR"
+    with open("Variables.var", 'w') as f:
+        f.write(connectionLanguage)
 
     
--- getConvertFile.py
+++ getConvertFile.py
@@ -7,44 +7,45 @@ Created on Fri May 25 16:11:58 2018
 
 import openpyxl
 
-typeDict = {"bool":"BOOL", "double":"LREAL", "float": "REAL", "int":"DINT", "unsigned int":"DWORD"}
+def convertToSt():
+    typeDict = {"bool":"BOOL", "double":"LREAL", "float": "REAL", "int":"DINT", "unsigned int":"DWORD"}
 
-convertLanguage = ""
-convertPrefixLanguage = "PROGRAM _CYCLE\n"
-newStructName = "g_ChannelDataFull"
-oldStructName = "g_ChannelData"
-convertMainLanguage = ""
-convertSuffixLanguage = "END_PROGRAM"
+    convertLanguage = ""
+    convertPrefixLanguage = "PROGRAM _CYCLE\n"
+    newStructName = "g_ChannelDataFull"
+    oldStructName = "g_ChannelData"
+    convertMainLanguage = ""
+    convertSuffixLanguage = "END_PROGRAM"
 
-# Parse excel
-fileName = "g_pChannelData.xlsx"
-workbook = openpyxl.load_workbook(fileName)
-sheet1 = workbook["Sheet1"]
-for i in range(2, sheet1.max_row+1):
-    cellForLongName = sheet1.cell(row = i, column = 4)
-    longName = cellForLongName.value
-    cellForType = sheet1.cell(row = i, column = 2)
-    typeName = cellForType.value
-    cellForShortName = sheet1.cell(row = i, column = 5)
-    shortName = cellForShortName.value
-    name = longName if len(longName) < 32 else shortName
-    if "[" not in name:
-        convertMainLanguage += "\t" + newStructName + "." + name \
-                                + " := " + oldStructName + "." + shortName + ";\n"
-    else:
-        index1 = name.find("[")
-        index2 = name.find("]")
-        num = int(name[index1+1: index2])
-        name = name[:index1]
-        shortNameIndex1 = shortName.find("[")
-        shortName = shortName[:shortNameIndex1]
-        for j in range(num+1):     
+    # Parse excel
+    fileName = "g_pChannelData.xlsx"
+    workbook = openpyxl.load_workbook(fileName)
+    sheet1 = workbook["Sheet1"]
+    for i in range(2, sheet1.max_row+1):
+        cellForLongName = sheet1.cell(row = i, column = 4)
+        longName = cellForLongName.value
+        cellForType = sheet1.cell(row = i, column = 2)
+        typeName = cellForType.value
+        cellForShortName = sheet1.cell(row = i, column = 5)
+        shortName = cellForShortName.value
+        name = longName if len(longName) < 32 else shortName
+        if "[" not in name:
             convertMainLanguage += "\t" + newStructName + "." + name \
-                                + "[" + str(j) + "]" + " := " + oldStructName \
-                                + "." + shortName + "[" + str(j) + "]" + ";\n"
+                                    + " := " + oldStructName + "." + shortName + ";\n"
+        else:
+            index1 = name.find("[")
+            index2 = name.find("]")
+            num = int(name[index1+1: index2])
+            name = name[:index1]
+            shortNameIndex1 = shortName.find("[")
+            shortName = shortName[:shortNameIndex1]
+            for j in range(num+1):     
+                convertMainLanguage += "\t" + newStructName + "." + name \
+                                    + "[" + str(j) + "]" + " := " + oldStructName \
+                                    + "." + shortName + "[" + str(j) + "]" + ";\n"
 
-convertLanguage = convertPrefixLanguage + convertMainLanguage + convertSuffixLanguage
+    convertLanguage = convertPrefixLanguage + convertMainLanguage + convertSuffixLanguage
 
-# To the file
-with open("Cyclic.st", 'w') as f:
-    f.write(convertLanguage)
+    # To the file
+    with open("Cyclic.st", 'w') as f:
+        f.write(convertLanguage)
